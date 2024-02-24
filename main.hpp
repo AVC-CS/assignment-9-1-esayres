0c3031443503a05ad6ddc522241d4e56ba688d5e
Elijah Sayres <elijahsayres@gmail.comm>
2024-02-23 20:10:38 -0800

    3rd Commit: sortnumbers() function working

--- main.hpp
+++ main.hpp
@@ -50,16 +50,48 @@ int getLength(struct Node *head){
     }
     return cnt;
 }
-struct Node *sortNumbers(struct Node *head)
-{
-    /*******************************
-     * Code your program here
-     *******************************/
+struct Node *sortNumbers(struct Node *head){
+    int length = getLength(head);
+    
+    if (length <= 1)
+        return head;
+
+    // bubble sort
+    for (int i = 0; i < length - 1; ++i) {
+        struct Node *current = head;
+        struct Node *nextNode = head->next;
+        struct Node *prev = NULL; // Keep track of the previous node for swapping
+
+        for (int j = 0; j < length - i - 1 && nextNode != NULL; ++j) {
+            // Compare values of adjacent nodes and swap if necessary
+            if (current->value > nextNode->value) {
+                // Swap the nodes
+                if (current == head) {
+                    head = swapNode(NULL, current);
+                } else {
+                    swapNode(prev, current);
+                }
+                
+                // Update prev for the next iteration
+                prev = nextNode;
+                nextNode = current->next;
+            } else {
+                // Move to the next pair of nodes
+                prev = current;
+                current = nextNode;
+                nextNode = nextNode->next;
+            }
+        }
+    }
+    return head;
 }
 
-struct Node *swapNode(struct Node *prev, struct Node *ptr)
-{
-    /*******************************
-     * Code your program here
-     *******************************/
+struct Node *swapNode(struct Node *prev, struct Node *ptr){
+    struct Node *nextNode = ptr->next;
+    ptr->next = nextNode->next;
+    nextNode->next = ptr;
+    if (prev != NULL) {
+        prev->next = nextNode;
+    }
+    return nextNode;
 }
